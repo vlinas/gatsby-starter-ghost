4ca329a8922bc09e1ff18fad343f7ddfb841642c
Linas <linas@genflow.com>
2021-05-18 16:00:20 +0300

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ try {
     }
 }
 
-if (process.env.NODE_ENV === `production` && config.siteUrl === `http://blog.shreddy.com/` && !process.env.SITEURL) {
+if (process.env.NODE_ENV === `production` && config.siteUrl === `http://blog.shreddy.com` && !process.env.SITEURL) {
     throw new Error(`siteUrl can't be localhost and needs to be configured in siteConfig. Check the README.`) // eslint-disable-line
 }
 
--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `http://blog.shreddy.com/`, // Site domain. Do not include a trailing slash!
+    siteUrl: `http://blog.shreddy.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
